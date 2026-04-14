ffcc3265b1784d64311c7fe9373a27df6618502a
Pigeooooon <S4172292@student.rmit.edu.au>
2026-04-14 12:19:45 +1000

    Prototype of the game
    Can feed the animals, although it dosent say what food is what, and which animal is which. Needs touch controls, need to fix a few bugs such as the food getting stuck when it given to the wrong animal

---
+++ SnackSwipeVer1/Assets/FoodSpawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    public GameObject[] foodPrefabs; // array to drag the three foods into
+    public Transform spawnPoint;    // Where the food should appear
+    
+    public static FoodSpawner Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        SpawnRandomFood();
+    }
+
+    public void SpawnRandomFood()
+    {
+        // Pick a random Food from the array
+        int randomIndex = Random.Range(0, foodPrefabs.Length);
+
+        // Create the food at the spawn points position
+        Instantiate(foodPrefabs[randomIndex], spawnPoint.position, Quaternion.identity);
+    }
+}
---
+++ SnackSwipeVer1/Assets/UniversalFood.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+
+
+
+// Note: script needs to be adjusted to be played on tablets
+    
+   
+public class UniversalFood : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private string likedAnimalTag;
+
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Collider2D currentAnimal; // Tracks if we are hovering over an animal
+
+
+    //Offset exists so the foods center dosent snap to mouse
+    void OnMouseDown()
+    {
+        offset = transform.position - GetMousePos();
+        isDragging = true;
+    }
+
+    // Have the food follow the mouse when dragging 
+    void OnMouseDrag()
+    {
+        transform.position = GetMousePos() + offset ;
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+
+        // If there is an animal within the food
+        if (currentAnimal != null)
+        {
+            // if the animal has the same tag that we declare
+            if (currentAnimal.CompareTag(likedAnimalTag))
+            {
+                // Print that the animal is happy destroy the food and spawn anotherfood
+                Debug.Log(currentAnimal.name + " ate the food and is happy :)" );
+                Destroy(gameObject);
+                FoodSpawner.Instance.SpawnRandomFood();
+            }
+            else
+            {
+                //Print the animal is sad and return the food to the middle of the screen 
+                Debug.Log(currentAnimal.name + " DIDNT ate the food and is sad :(");
+               
+            }
+        }
+    }
+
+    // When the foods Box collider enters the animals box collider, set "currentAnimal" to other
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        currentAnimal = other;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        currentAnimal = null;
+    }
+
+    //Get mouse pos in the 2D space
+    Vector3 GetMousePos()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+}
